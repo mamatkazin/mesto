d1c2570f41bf5eb80c23dd830368f589e560fa4b
Ильнур Маматказин <ilnur.mamatkazin@gmail.com>
2020-10-20 14:45:56 +0300

    First commit

---
+++ script/index.js
@@ -0,0 +1,38 @@
+let profile = document.querySelector(".profile");
+let popup = document.querySelector(".popup");
+
+let editButton = profile.querySelector(".profile__button-edit");
+let closeButton = popup.querySelector(".popup__button-close");
+let submitButton = popup.querySelector(".popup__button");
+
+let name = popup.querySelector(".popup__input-name");
+let descr = popup.querySelector(".popup__input-descr");
+
+let title = profile.querySelector(".profile__title");
+let subtitle = profile.querySelector(".profile__subtitle");
+
+editButton.addEventListener("click", formLoad);
+closeButton.addEventListener("click", formClose);
+submitButton.addEventListener("click", formSubmitHandler);
+
+function formLoad() {
+  name.value = title.textContent;
+  descr.value = subtitle.textContent;
+
+  popup.classList.add("popup_opened");
+}
+
+function formClose() {
+  popup.classList.remove("popup_opened");
+}
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
+  title.textContent = name.value;
+  subtitle.textContent = descr.value;
+
+  popup.classList.remove("popup_opened");
+}
