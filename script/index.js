780844ed15ce53d49fcfbb61398f61203dfa14e1
Ильнур Маматказин <ilnur.mamatkazin@gmail.com>
2020-10-20 16:22:06 +0300

    Доблен файл .nojekyll

--- script/index.js
+++ script/index.js
@@ -1,29 +1,29 @@
-let profile = document.querySelector(".profile");
-let popup = document.querySelector(".popup");
+let profile = document.querySelector('.profile');
+let popup = document.querySelector('.popup');
 
-let editButton = profile.querySelector(".profile__button-edit");
-let closeButton = popup.querySelector(".popup__button-close");
-let submitButton = popup.querySelector(".popup__button");
+let editButton = profile.querySelector('.profile__button-edit');
+let closeButton = popup.querySelector('.popup__button-close');
+let submitButton = popup.querySelector('.popup__button');
 
-let name = popup.querySelector(".popup__input-name");
-let descr = popup.querySelector(".popup__input-descr");
+let name = popup.querySelector('.popup__input-name');
+let descr = popup.querySelector('.popup__input-descr');
 
-let title = profile.querySelector(".profile__title");
-let subtitle = profile.querySelector(".profile__subtitle");
+let title = profile.querySelector('.profile__title');
+let subtitle = profile.querySelector('.profile__subtitle');
 
-editButton.addEventListener("click", formLoad);
-closeButton.addEventListener("click", formClose);
-submitButton.addEventListener("click", formSubmitHandler);
+editButton.addEventListener('click', formLoad);
+closeButton.addEventListener('click', formClose);
+submitButton.addEventListener('click', formSubmitHandler);
 
 function formLoad() {
   name.value = title.textContent;
   descr.value = subtitle.textContent;
 
-  popup.classList.add("popup_opened");
+  popup.classList.add('popup_opened');
 }
 
 function formClose() {
-  popup.classList.remove("popup_opened");
+  popup.classList.remove('popup_opened');
 }
 
 // Обработчик «отправки» формы, хотя пока
@@ -34,5 +34,5 @@ function formSubmitHandler(evt) {
   title.textContent = name.value;
   subtitle.textContent = descr.value;
 
-  popup.classList.remove("popup_opened");
+  popup.classList.remove('popup_opened');
 }
