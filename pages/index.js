fb6139532f584973ffb9e56c813edd317c29579b
Ильнур Маматказин <ilnur.mamatkazin@gmail.com>
2020-10-21 16:26:57 +0300

    Отправка на ревью

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,36 @@
-let profile = document.querySelector(".profile");
-let popup = document.querySelector(".popup");
+let profile = document.querySelector('.profile');
+let popup = document.querySelector('.popup');
 
-let editButton = profile.querySelector(".profile__button-edit");
-let closeButtonPopup = popup.querySelector(".popup__button-close");
-let submitButtonPopup = popup.querySelector(".popup__button");
+let editButton = profile.querySelector('.profile__button-edit');
+let closeButtonPopup = popup.querySelector('.popup__button-close');
+let submitButtonPopup = popup.querySelector('.popup__button');
 
-let name = popup.querySelector(".popup__input-name");
-let descr = popup.querySelector(".popup__input-descr");
+let name = popup.querySelector('.popup__input-name');
+let descr = popup.querySelector('.popup__input-descr');
 
-let title = profile.querySelector(".profile__title");
-let subtitle = profile.querySelector(".profile__subtitle");
+let title = profile.querySelector('.profile__title');
+let subtitle = profile.querySelector('.profile__subtitle');
 
-editButton.addEventListener("click", formLoad);
-closeButtonPopup.addEventListener("click", formClose);
-submitButtonPopup.addEventListener("click", formSubmitHandler);
+editButton.addEventListener('click', formLoad);
+closeButtonPopup.addEventListener('click', formClose);
+submitButtonPopup.addEventListener('click', formSubmit);
 
 function formLoad() {
   name.value = title.textContent;
   descr.value = subtitle.textContent;
 
-  popup.classList.add("popup_opened");
+  popup.classList.add('popup_opened');
 }
 
 function formClose() {
-  popup.classList.remove("popup_opened");
+  popup.classList.remove('popup_opened');
 }
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler(evt) {
+function formSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
 
   title.textContent = name.value;
   subtitle.textContent = descr.value;
 
-  popup.classList.remove("popup_opened");
+  popup.classList.remove('popup_opened');
 }
