d7d6eef165029aabe3c421e12eed0897d65f481f
Ильнур Маматказин <ilnur.mamatkazin@gmail.com>
2020-10-26 17:28:26 +0300

    Анимировал лайки, сверстал корзину

---
+++ scripts/index1.js
@@ -0,0 +1,93 @@
+const profile = document.querySelector('.profile');
+const editButton = profile.querySelector('.profile__button-edit');
+const addButton = profile.querySelector('.profile__button-add');
+
+const title = profile.querySelector('.profile__title');
+const subtitle = profile.querySelector('.profile__subtitle');
+
+const submitButtonAdd = document.querySelector('.popup_type-form_add');
+const submitButtonEdit = document.querySelector('.popup_type-form_edit');
+
+const elements = document.querySelector('.elements');
+const likeButtons = elements.querySelectorAll('.button-like');
+const deleteButtons = elements.querySelectorAll('.button-delete');
+const images = elements.querySelectorAll('.elements__image');
+
+let closeButton;
+
+function activationFormElements(className) {
+  let p = document.querySelector(className);
+  p.classList.add('popup_opened');
+
+  closeButton = p.querySelector('.popup_opened .button-close');
+  closeButton.addEventListener('click', formClose, { once: true });
+}
+
+function loadFormEdit() {
+  activationFormElements('.popup_type-form_edit');
+
+  let name = document.querySelector('.popup_opened .popup__input-name');
+  let descr = document.querySelector('.popup_opened .popup__input-descr');
+
+  name.value = title.textContent;
+  descr.value = subtitle.textContent;
+}
+
+function loadFormAdd() {
+  activationFormElements('.popup_type-form_add');
+}
+
+function formClose(e) {
+  e.currentTarget.closest('.popup').classList.remove('popup_opened');
+}
+
+function formSubmitAdd(e) {
+  e.preventDefault();
+
+  formClose(e);
+}
+
+function formSubmitEdit(e) {
+  e.preventDefault();
+
+  let p = this.closest('.popup');
+  let name = p.querySelector('.popup__input-name');
+  let descr = p.querySelector('.popup__input-descr');
+
+  title.textContent = name.value;
+  subtitle.textContent = descr.value;
+
+  formClose(e);
+}
+
+function liked() {
+  this.classList.toggle('button-like_liked');
+}
+
+function deleted() {
+  alert(this);
+}
+
+function loadFormPicture(e) {
+  activationFormElements('.popup_type-form_picture');
+
+  let image = document.querySelector('.popup_opened .figure__image');
+  image.setAttribute('src', e.currentTarget.getAttribute('src'));
+}
+
+editButton.addEventListener('click', loadFormEdit);
+addButton.addEventListener('click', loadFormAdd);
+submitButtonAdd.addEventListener('submit', formSubmitAdd);
+submitButtonEdit.addEventListener('submit', formSubmitEdit);
+
+likeButtons.forEach((b) => {
+  b.addEventListener('click', liked);
+});
+
+deleteButtons.forEach((b) => {
+  b.addEventListener('click', deleted);
+});
+
+images.forEach((i) => {
+  i.addEventListener('click', loadFormPicture);
+});
